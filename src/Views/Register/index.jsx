4ee91e8c3511eb2a85b7667fec8f034383cd4887
Armando Prado <armap1999@hotmail.com>
2021-11-24 18:00:35 -0600

    /feat new RegisterForm component

--- src/Views/Register/index.jsx
+++ src/Views/Register/index.jsx
@@ -1,9 +1,19 @@
 import './styles.css'
+import RegisterForm from '../../components/RegisterForm'
 
 const Register = () => {
   return (
-    <div>
-      <p>Registro</p>
+    <div className="general-register-container">
+      <div>
+        <img
+          className="image-container-register"
+          src="assets/img/Logos/LogoPrincipalNoFondo.png"
+          alt="Workick-logo-register"
+        />
+      </div>
+      <div>
+        <RegisterForm></RegisterForm>
+      </div>
     </div>
   )
 }
