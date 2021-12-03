731fffc2c70c02dedb9671e0836f10e5cc8a226e
Armando Prado <armap1999@hotmail.com>
2021-12-02 19:40:04 -0600

    /feat: was added the login desing

--- src/components/LoginForm/index.jsx
+++ src/components/LoginForm/index.jsx
@@ -1,24 +1,56 @@
+import { useState } from 'react'
 import './styles.css'
 
 const LoginForm = () => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const onChangeEmail = (event) => {
+    setEmail(event.target.value)
+  }
+
+  const onChangePassword = (event) => {
+    setPassword(event.target.value)
+  }
+
   return (
     <div className="form-container-login">
       <form action="">
-        <div className="container-image-icon-login-form">
-          <img
-            className="image-icon-login-form"
-            src="/assets/img/Logos/LogoIcono.png"
-            alt="logo-login"
-          />
-          <p>orkick</p>
-          <p>Bienvenido</p>
-          <input type="email" placeholder="Correo electronico" />
-          <input type="password" placeholder="Contraseña" />
+        <div className="title-container-login-form">
+          <div className="container-image-icon-login-form">
+            <img
+              className="image-icon-login-form"
+              src="/assets/img/Logos/LogoIcono.png"
+              alt="logo-login"
+            />
+          </div>
+          <p className="part-text-title-login">orkick</p>
         </div>
-        <button type="submit">Iniciar Sesión</button>
+        <p className="bienvenido-text-login">Bienvenido</p>
+        <input
+          className="input-login-form"
+          type="email"
+          placeholder="Correo electronico"
+          value={email}
+          onChange={onChangeEmail}
+        />
+        <input
+          className="input-login-form"
+          type="password"
+          placeholder="Contraseña"
+          value={password}
+          onChange={onChangePassword}
+        />
+        <div className="buton-container-login">
+          <button className="button-login-form" type="submit">
+            Iniciar Sesión
+          </button>
+        </div>
       </form>
-      <p>¿No tienes una cuenta?</p>
-      <button>Crear una Cuenta</button>
+      <div className="footer-container-login-form">
+        <p className="some-text-footer-login">¿No tienes una cuenta?</p>
+        <button className="footer-button-login-form">Crear una Cuenta</button>
+      </div>
     </div>
   )
 }
