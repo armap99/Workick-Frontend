28ca012887116670065487c2fdeab843de2ec256
Armando Prado <luis.prado@hemoeco.com>
2021-12-20 13:01:21 -0600

    /feat: the navigation bar was separated into two news componentes

--- src/components/NavigationBar/index.jsx
+++ src/components/NavigationBar/index.jsx
@@ -1,61 +1,13 @@
 import { useContext } from 'react' // para usar el contexto
-import { useHistory } from 'react-router-dom' // para usar el contexto
-import { AppContext } from '../../data/state'
+import { AppContext } from '../../data/state' // para usar el contexto
 
-import './styles.css'
+import NavigationBarLogIn from './components/NavigationBarLogIn'
+import NavigationBarNoLogIn from './components/NavigationBarNoLogIn'
 
 const NavigationBar = () => {
   const { state } = useContext(AppContext)
-  const history = useHistory()
 
-  const redirectLogin = () => {
-    history.push('login')
-  }
-
-  /* return (
-    <>
-      {state.nombre ? sndnsndsind : sndisndisnd} 
-    </>
-  ) */
-  if (state.nombre) {
-    return (
-      <nav className="navigation-bar-container">
-        <ul>
-          <li className="container-li-bienvedios-nav">
-            <p className="text-nav-bar">Bienvenidos a Workick</p>
-          </li>
-          <li className="container-li-login container-li-icon">
-            <p className="text-login-nav">{state.nombre} ▼</p>
-          </li>
-        </ul>
-      </nav>
-    )
-  } else {
-    return (
-      <nav className="navigation-bar-container">
-        <ul>
-          <li className="container-li-bienvedios-nav">
-            <p className="text-nav-bar">Bienvenidos a Workick</p>
-          </li>
-          <li className="container-li-login">
-            <div className="login-container-icon">
-              <img
-                onClick={redirectLogin}
-                className="login-icon"
-                src="assets/img/Icons/login.svg"
-                alt="loginIcon"
-              />
-            </div>
-          </li>
-          <li className="container-li-login container-li-icon">
-            <p onClick={redirectLogin} className="text-login-nav">
-              Iniciar Sesión
-            </p>
-          </li>
-        </ul>
-      </nav>
-    )
-  }
+  return <>{state.nombre ? <NavigationBarLogIn /> : <NavigationBarNoLogIn />}</>
 }
 
 export default NavigationBar
